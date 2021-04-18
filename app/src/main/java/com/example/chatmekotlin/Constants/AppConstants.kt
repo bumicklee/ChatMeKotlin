d36cac9c8b2df0a28d6766b20e7a0fd1b39c53c1
bumicklee <bruno.leebi@gmail.com>
2021-04-19 00:26:08 +0900

    설정정보 변경

--- app/src/main/java/com/example/chatmekotlin/Constants/AppConstants.kt
+++ app/src/main/java/com/example/chatmekotlin/Constants/AppConstants.kt
@@ -5,7 +5,7 @@ interface AppConstants {
     companion object {
 
         const val CHANNEL_ID = "Message Channel"
-        const val SERVER_KEY="AAAATcwS_uQ:APA91bGajquqfuymyKu3OjQGNGR_GnV-xds77-SY-4aw9w8hMUctmFerP0rHUNsHVl38-TO15l-QIEJd5Vh1_YA7NIQ_7mhgnvGpEMMDi4SncqHG3XUrwCiCtDDx7aYPoUJdxabQAEuk"
+        const val SERVER_KEY="AAAAJfW6U_8:APA91bFxNeR2U9TE3ux0N8B26bCXfXUDy23eErOgJuu63V3ezkjYcs7Y74YMPff8p0JoncnzNp7hxZgj7DX1FaBs6nAphrbWPnShOzWuZrJFuq4S2P2Dy_ktdKjQhQcZK5324FVcL_dO"
         const val NOTIFICATION_URL = "https://fcm.googleapis.com/fcm/send"
         const val PATH = "/ProfileImage/image"
         const val UID = "ZyvJrMphIbbZQth0E7n1KmvFLWu1"
